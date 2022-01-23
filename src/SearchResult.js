594c16f135d91afe695d2e367da103a4478d8088
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-01-23 22:48:39 +0000

    Added phonetics feature

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function SearchResult(props) {
   if (props.results) {
     return (
       <div className="SearchResult">
         <h1>{props.results.word}</h1>
-        <p>{props.results.phonetic}</p>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetic} />
+            </div>
+          );
+        })}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
