36be01870d1437036d0be3da1ff4eac243a5e633
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-02-01 08:28:28 +0000

    fixed build

--- src/Photos.js
+++ src/Photos.js
@@ -8,7 +8,7 @@ export default function Photos(props) {
           return (
             <div className="image-column" key={index}>
               <a href={photo.src.original} target="_blank" rel="noreferrer">
-                <img src={photo.src.portrait} />
+                <img src={photo.src.portrait} alt={photo.alt} />
               </a>
             </div>
           );
